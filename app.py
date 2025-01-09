47fbf85b3bdd0c73ac6097493f08f9a9de652d8d
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-01-09 20:31:46 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print(f"Addition: {add(5, 3)}")
+    print(f"Subtraction: {subtract(5, 3)}")
